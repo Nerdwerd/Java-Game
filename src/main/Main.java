deb06483ca107bb51abe3f6e28bc55cb4ea6218c
Nerdwerd <50635875+Nerdwerd@users.noreply.github.com>
2020-01-14 11:43:50 -0600

    Basic Window Setup

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,45 @@
 package main;
 
-public class Main {
+import java.awt.*;
 
+public class Main extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 3196520274556633778L;
+
+	public final int WIDTH = 400;
+	public final int HEIGHT = 400;
+	private Thread thread;
+	private boolean running = false;
+	
 	public static void main(String[] args) {
-		
+		new Main();
 
 	}
+	
+	public Main() {
+		new Window(WIDTH, HEIGHT, "Epic Game", this);
+	}
+	
+	public synchronized void start() {
+		thread = new Thread(this);
+		thread.start();
+		running = true;
+	}
 
+	public synchronized void stop() {
+		try {
+			thread.join();
+			running = false;
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void run() {
+		
+		
+	}
+	
 }
