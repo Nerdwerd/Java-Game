55a9543e239c436b877ffa94dc353b96a3ac5601
Nerdwerd <50635875+Nerdwerd@users.noreply.github.com>
2020-01-14 13:19:48 -0600

    Worked on FPS and some Rendering

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,14 @@
 package main;
 
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 public class Main extends Canvas implements Runnable {
 
 	private static final long serialVersionUID = 3196520274556633778L;
 
-	public final int WIDTH = 400;
-	public final int HEIGHT = 400;
+	public final int WIDTH = 500;
+	public final int HEIGHT = 500;
 	private Thread thread;
 	private boolean running = false;
 	
@@ -39,7 +40,50 @@ public class Main extends Canvas implements Runnable {
 	
 	public void run() {
 		
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1) {
+				ticks();
+				delta--;
+			}
+			if(running) {
+				render();
+			}
+			frames++;
+			if(System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();		
+	}
+	
+	private void ticks() {
 		
 	}
 	
+	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		g.dispose();
+		bs.show();
+	}
 }
